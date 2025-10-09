a153d28172d461e3259eb8a525e7cde79f84a126
Leandro Amaro Garcia <leandroamarogarcia@leandros-mbp.lan>
2025-10-09 18:25:10 -0300

    prod

--- models/products.js
+++ models/products.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
   name: { type: String },
+  image: {type: String},
   categories: [{ type: Schema.Types.ObjectId, ref: "Category" }]
 });
 
